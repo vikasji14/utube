15d94c91f53043aac6effcae69c1768dc9f13b95
githubinfovikas <vikasdbg453@gmail.com>
2024-07-06 17:06:52 +0530

    refactor: Update multer middleware to use in-memory storage for file uploads

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -11,8 +11,8 @@ import multer from "multer";
 
 
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+// const upload = multer({ storage: storage });
   
-// export const upload = multer({ 
-//     storage,
-// })
+export const upload = multer({ 
+    storage,
+})
